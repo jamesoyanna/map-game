b0b8a55c7d958b847008fd37e2584fba297a5e30
eshoiza <engrjayt200@gmail.com>
2019-11-28 17:57:10 +0100

    implemented a countdown timer

--- app.js
+++ app.js
@@ -175,11 +175,41 @@ countryStates.forEach((state) => {
 
 /***
  * IMPLEMENT TIMER FUNCTIONALITY
+ * Check out this count down timer I have just inplemented
  */
+function countdown(elementName, minutes, seconds) {
+  var element, endTime, hours, mins, msLeft, time;
 
-let timerEle = document.getElementById('timer');
-let timeCount = 0;
-setInterval(() => {
-  timeCount = timeCount + 1;
-  timerEle.innerText = timeCount;
-}, 1000);
+  function twoDigits(n) {
+    return n <= 9 ? "0" + n : n;
+  }
+
+  function updateTimer() {
+    msLeft = endTime - +new Date();
+    if (msLeft < 1000) {
+      element.innerHTML = "Time is up!";
+    } else {
+      time = new Date(msLeft);
+      hours = time.getUTCHours();
+      mins = time.getUTCMinutes();
+      element.innerHTML =
+        (hours ? hours + ":" + twoDigits(mins) : mins) +
+        ":" +
+        twoDigits(time.getUTCSeconds());
+      setTimeout(updateTimer, time.getUTCMilliseconds() + 500);
+    }
+  }
+
+  element = document.getElementById(elementName);
+  endTime = +new Date() + 1000 * (60 * minutes + seconds) + 500;
+  updateTimer();
+}
+countdown("ten-countdown", 5, 0);
+
+
+// let timerEle = document.getElementById('timer');
+// let timeCount = 0;
+// setInterval(() => {
+//   timeCount = timeCount + 1;
+//   timerEle.innerText = timeCount;
+// }, 1000);
