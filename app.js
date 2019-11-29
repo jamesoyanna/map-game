6f7bf69f5df8c1225c7a7033a95f141de875f242
spaceofmiah <geek_200@outlook.com>
2019-11-29 09:44:57 +0100

    Hide draggable on time out, fix bug, comment code
    Draggable object is automatically hidden on time out,
    remove drop event from drop zones.
    
    Fix bug :: the displayed draggable state weren't randomly displayed
    
    Comment out code base ( esp. countdown function )

--- app.js
+++ app.js
@@ -6,9 +6,12 @@ const states = [
     "gombe",        "bauchi",   "kaduna",     "kebbi",      "niger",
     "sokoto",       "zamfara",  "kano",       "jigawa",     "yobe",
     "borno",        "taraba",   "adamawa",    "abuja",      "ogun",
-    "imo",          "abia",     "ebonyi",     "nassarawa",   "katsina",
+    "imo",          "abia",     "ebonyi",     "nassarawa",  "katsina",
     "bayelsa"
 ];
+
+// retrieve all states available on map DOM object
+let countryStates = document.querySelectorAll('.state');
 
 
 /**
@@ -30,7 +33,7 @@ const generate_rand_color = () => {
  */
 const next_state = () => {
   let num_of_state_remaining = Math.floor(Math.random() * states.length)
-  return states.pop(num_of_state_remaining)
+  return states.splice(num_of_state_remaining, 1);
 }
 
 
@@ -43,6 +46,8 @@ const change_draggable_prop = (draggable) => {
   draggable.id = `drag-${state}`
   draggable.innerText = state
 }
+
+
 
 /********************************************************************************** */
 //  QUICK BRIEF
@@ -68,41 +73,42 @@ const change_draggable_prop = (draggable) => {
 const ondrag_handler = (event) => {
   // change th color
   event
-    .currentTarget
-    .style
+  .currentTarget
+  .style
     .background = "#07fff4";
-
-  event 
+    
+    event 
     .currentTarget
     .style 
     .transform = `translate(${event.screenX}, ${event.screenY})`
-}
-
-
-
-const ondragstart_handler = (event) => {
-  // enable us to retrieve the element that is being dragged
-  // when ever we drop it
-  event
+  }
+  
+  
+  
+  const ondragstart_handler = (event) => {
+    // enable us to retrieve the element that is being dragged
+    // when ever we drop it
+    event
     .dataTransfer
     .setData('text/plain', event.target.id);
-
-  console.log("drag start");
-}
-
-
-let draggable = document.querySelector('.draggable');
-draggable.addEventListener('dragstart', ondragstart_handler);
-draggable.addEventListener('drag', ondrag_handler);
-
-
-
-
-/**
- * Allows dropzones to accept draggable as it browsers disables
- * it by default
- * @param {DragOver} event a dragover event
- */
+    
+    console.log("drag start");
+  }
+  
+  
+  let draggable = document.querySelector('.draggable');
+  draggable.addEventListener('dragstart', ondragstart_handler);
+  draggable.addEventListener('drag', ondrag_handler);
+  change_draggable_prop(draggable);
+  
+  
+  
+  
+  /**
+   * Allows dropzones to accept draggable as it browsers disables
+   * it by default
+   * @param {DragOver} event a dragover event
+   */
 const ondragover_handler = (event) => {
   event.preventDefault();
   console.log("accept drop")
@@ -161,9 +167,8 @@ const ondrop_handler = (event) => {
 
 
 
- // the elements that will be used as dropzones are svg path
+// the elements that will be used as dropzones are svg path
 // there's need to iterate through and then set the ondragover_handler
-let countryStates = document.querySelectorAll('.state');
 countryStates.forEach((state) => {
   state.addEventListener('dragover', ondragover_handler);
   state.addEventListener('drop', ondrop_handler);
@@ -177,34 +182,99 @@ countryStates.forEach((state) => {
  * IMPLEMENT TIMER FUNCTIONALITY
  * Check out this count down timer I have just inplemented
  */
-function countdown(elementName, minutes, seconds) {
-  var element, endTime, hours, mins, msLeft, time;
+const countdown = (elementName, minutes, seconds) => {
+  let element, endTime, hours, mins, 
+      msLeft, time, chosenTimeInMinutes, 
+      gamePlayMinutesInSeconds;
 
-  function twoDigits(n) {
-    return n <= 9 ? "0" + n : n;
-  }
+  // DOM element to display timer
+  element = document.getElementById(elementName);
 
-  function updateTimer() {
-    msLeft = endTime - +new Date();
+  /**
+   * 60 seconds == 1 minutes :::::  60 * minutes + seconds  
+   * 
+   * given 5 minutes 8 seconds
+   * 60 * 5 + 8 == 308            ----- RESULT 1
+   */
+  chosenTimeInMinutes = 60 * minutes + seconds
+
+  /**
+   * 1000 miliseconds makes a seconds ::::: 1000 * choosenTimeInMinutes 
+   * 
+   * using RESULT 1
+   * 1000 * 308 === 308000
+   */
+  gamePlayMinutesInSeconds = 1000 * chosenTimeInMinutes
+
+  // compute real time from the values passed in :: minutes & seconds
+  endTime = gamePlayMinutesInSeconds + 500 + new Date().getTime();
+
+
+
+  /**
+   * returns a two numerical value for a single numerical 
+   * value. if value passed is already more than one 
+   * numerical value, then the value is returned as is.
+   * 
+   * e.g 
+   *    if 4 which is a single numerical value is passed
+   *    function will return `04`
+   *   
+   *    if 10 which is two numerical value is passed
+   *    function will return `10`
+   * 
+   * @param {Integer} n : Numerical value to be doubled
+   */
+  const twoDigits = n => n <= 9 ? "0" + n : n;
+
+
+  /**
+   * counts down the passed time
+   */
+  const updateTimer= () => {
+    msLeft = endTime - new Date().getTime();
+
     if (msLeft < 1000) {
       element.innerHTML = "Game Over!";
-    } else {
+
+      draggable.style.display = 'none';
+
+      // the elements that will be used as dropzones are svg path
+      // there's need to iterate through and then set the ondragover_handler
+      countryStates.forEach((state) => {
+        state.removeEventListener('dragover', ondragover_handler);
+        state.removeEventListener('drop', ondrop_handler);
+      });
+
+    } 
+
+    else {
       time = new Date(msLeft);
       hours = time.getUTCHours();
       mins = time.getUTCMinutes();
-      element.innerHTML =
-        (hours ? hours + ":" + twoDigits(mins) : mins) +
-        ":" +
-        twoDigits(time.getUTCSeconds());
-      setTimeout(updateTimer, time.getUTCMilliseconds() + 500);
+
+      // compute the time value to be displayed
+      // (step 1) if we have hours greater than 0, then display
+      // format would be `hours:minutes:seconds` 
+
+      // (step 2) if we have hours less than or equal to 0, then 
+      // display format would be `minute:seconds`
+      currentCountDownTime = (
+        hours > 0 ? 
+            `${hours}:${twoDigits(mins)}:${twoDigits(time.getUTCSeconds())}`        
+          :            
+            `${twoDigits(mins)}:${twoDigits(time.getUTCSeconds())}`
+      );
+
+      element.innerHTML = currentCountDownTime;
+      setTimeout(updateTimer, 1000);
     }
   }
 
-  element = document.getElementById(elementName);
-  endTime = +new Date() + 1000 * (60 * minutes + seconds) + 500;
+  
   updateTimer();
 }
-countdown("ten-countdown", 5, 0);
+countdown("countdown", 0, 20);
 
 
 // let timerEle = document.getElementById('timer');
